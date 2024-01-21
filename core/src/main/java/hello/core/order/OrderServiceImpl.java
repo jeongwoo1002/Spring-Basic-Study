1fa0ba5460c28f6c47c8e3478e45578a0acd306a
jeongwoo1002 <kjw4016@naver.com>
2024-01-21 18:32:42 +0900

    thrid commit

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,22 +3,24 @@ package hello.core.order;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.discount.DiscountPolicy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
 
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;  //인터페이스에만 의존하도록 코드 변경
   //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 인터페이스뿐만 아니라 구체 클래스도 함께의존하고있음 DIP위반
   //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //OCP위반
-
-  @Autowired
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+/*  @RequiredArgsConstructor 이걸 사용하면 아래 코드가 들어가 있어서 쓸 필요 없음
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
-  }
+  }*/
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
