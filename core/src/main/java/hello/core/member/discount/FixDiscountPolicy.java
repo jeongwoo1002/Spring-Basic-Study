f04509b8c93289c46cfa21134183488d68434c85
jeongwoo1002 <kjw4016@naver.com>
2024-01-22 17:40:45 +0900

    fourth commit

--- core/src/main/java/hello/core/member/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/member/discount/FixDiscountPolicy.java
@@ -2,7 +2,12 @@ package hello.core.member.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+@Primary
 public class FixDiscountPolicy implements DiscountPolicy{
   
   private int discountFixAmount = 1000; //1000원 할인
@@ -13,7 +18,6 @@ public class FixDiscountPolicy implements DiscountPolicy{
       return discountFixAmount;
     }else {
       return 0;
-
     }
   }
 }
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,10 +5,11 @@ import hello.core.member.MemberRepository;
 import hello.core.member.discount.DiscountPolicy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
 
@@ -16,11 +17,12 @@ public class OrderServiceImpl implements OrderService {
   private final DiscountPolicy discountPolicy;  //인터페이스에만 의존하도록 코드 변경
   //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 인터페이스뿐만 아니라 구체 클래스도 함께의존하고있음 DIP위반
   //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //OCP위반
-/*  @RequiredArgsConstructor 이걸 사용하면 아래 코드가 들어가 있어서 쓸 필요 없음
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//  @RequiredArgsConstructor 이걸 사용하면 아래 코드가 들어가 있어서 쓸 필요 없음
+  @Autowired
+  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
-  }*/
+  }
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
